36dad011c79b1d86b6cc08a2ecd273f2bab1c593
Alaguprabha <alaguprabha@gmail.com>
2022-06-20 20:16:34 -0400

    this is commit5

--- src/test/java/StepDefination/Agreetoconditionspage.java
+++ src/test/java/StepDefination/Agreetoconditionspage.java
@@ -36,6 +36,7 @@ public void the_user_enters_the_country_and_click_on_agree_to_conditions_and_cli
 	//Thread.sleep(2000);
 	agreetocondpageobj.proceedbuttonclick();
 	System.out.println("Clicking on proceed button");
+	System.out.println("Clicking on proceed button1");
 	
 	
 	
